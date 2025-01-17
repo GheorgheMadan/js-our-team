352070a5bc602dde22f36d064b792dc0d4d1538b
Gheorghe <madangheorghe.30@gmail.com>
2025-01-17 14:23:22 +0100

    verify for

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png"
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png"
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png"
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png"
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png"
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png"
+  }
+];
+
+const outputCard = document.getElementById('output')
+
+let cards = ''
+// creo un ciclo for per stampare in pagina le card 
+for(let i = 0; i < teamMembers.length; i++){
+  let membroIesimo = teamMembers[i];
+  console.log(membroIesimo)
+}
